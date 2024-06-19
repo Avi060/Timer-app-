abd11a17a185a16c2eb95fbef61e74fc59fb733a
Avinash <avinash.sm2022@vitstudent.ac.in>
2024-06-19 18:30:10 +0530

    timer app completed

--- app/src/main/java/com/example/timerapp/MainActivity.kt
+++ app/src/main/java/com/example/timerapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.timerapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +15,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
+
+
+    fun clickHandler_1(view: View) {
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "Milk")
+            putExtra(AlarmClock.EXTRA_LENGTH, 180)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, false)
+        }
+        startActivity(intent)
+    }
+    fun clickHandler_2(view: View) {
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_LENGTH, 120)
+            putExtra(AlarmClock.EXTRA_MESSAGE, "Water Motor")
+            putExtra(AlarmClock.EXTRA_SKIP_UI, false)
+        }
+        startActivity(intent)
+
+    }
+    fun clickHandler_3(view: View) {
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "toaster")
+            putExtra(AlarmClock.EXTRA_LENGTH, 240)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, false)
+        }
+        startActivity(intent)
+
     }
 }
